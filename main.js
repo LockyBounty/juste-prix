c134e1b8d967ea21100ffaee24f462e426707f06
JadFays <faysaljadron@gmail.com>
2018-10-04 12:34:57 +0200

    final

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-let aleatoire = () => {
-    
+let min = 20;
+let max = 80;
+
+let aleatoire = ()=> {
+    let i = Math.floor(Math.random() * ((max + 1) - min) + min);
+    return i;
 }
+
+let check = ()=> {
+    let prixEntree;
+    let prix = aleatoire();
+    let count = 0;
+    while(prixEntree !== prix) {
+        prixEntree = prompt('Entrez un nombre !');
+        if(prixEntree < prix) {
+            alert(`C'EST PLUS !`);
+        } else if(prixEntree > prix) {
+            alert(`C'EST MOINS !`);
+
+        } else {
+            break;
+        }
+        count++;
+    }
+    confirm(`C'est juste tu as trouvé en ${count} coups`);
+}
+
+check();
